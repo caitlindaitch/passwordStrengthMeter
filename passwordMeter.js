fc6b078d2dead1e3a7f28e12f7c86828d4e74da4
Caitlin Daitch <caitlin.daitch@gmail.com>
2016-12-29 20:28:09 -0500

    Show rating

--- passwordMeter.js
+++ passwordMeter.js
@@ -2,12 +2,14 @@ var password = document.getElementById('password');
 
 password.addEventListener('keyup', function(){
 	var passwordArray = password.value.split('');
+	var totalScore = 0;
 
 	var rating = {
-		specialChar: 0,
+		number: 0,
 		lowercase: 0,
 		uppercase: 0,
-		numbers: 0
+		specialChar: 0,
+		total: 0
 	}
 
 	var validation = {
@@ -22,20 +24,43 @@ password.addEventListener('keyup', function(){
 		isUppercase: function(val){
 			var pattern = /[A-Z]/;
 			return pattern.test(val);
+		},
+		isSpecialChar: function(val){
+			var pattern = /^[!@#\$%\^\&*\)\(+=._-]+$/g;
+			return pattern.test(val);
 		}
 	}
 
 	for (var i=0; i<passwordArray.length; i++){
 		if (validation.isNumber(passwordArray[i])){
-			rating.numbers = 1;
+			rating.number = 1;
 		} else if (validation.isLowercase(passwordArray[i])){
 			rating.lowercase = 1;
 		} else if (validation.isUppercase(passwordArray[i])){
 			rating.uppercase = 1;
+		} else if (validation.isSpecialChar(passwordArray[i])){
+			rating.specialChar = 1;
 		}
 	}
 
-	console.log("Uppercase " + rating.uppercase);
-	console.log("Lowercase " + rating.lowercase);
-	console.log("Numbers " + rating.numbers);
+	function assessTotalScore(){
+		var ratingElement = document.querySelector(".rating");
+		rating.total = rating.number + rating.lowercase + rating.uppercase + rating.specialChar;
+
+		if (rating.total === 1){
+			ratingElement.innerHTML = "Weak";
+			ratingElement.classList.remove("moderatePassword", "strongPassword");
+			ratingElement.classList.add("weakPassword");
+		} else if (rating.total === 2){
+			ratingElement.innerHTML = "Moderate";
+			ratingElement.classList.remove("weakPassword", "strongPassword");
+			ratingElement.classList.add("moderatePassword");
+		} else if (rating.total === 3){
+			ratingElement.innerHTML = "Strong";
+			ratingElement.classList.remove("weakPassword", "moderatePassword");
+			ratingElement.classList.add("strongPassword");
+		}
+	}
+
+	assessTotalScore();
 });
